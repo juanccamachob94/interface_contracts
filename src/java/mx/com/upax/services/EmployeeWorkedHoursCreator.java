4b4cadc24a2c4826ce311e57dd7cbf2ceb0decc7
Juan Camacho <juanccamachob94@gmail.com>
2020-08-23 12:30:11 -0500

    employee worked hours creator service added

---
+++ src/java/mx/com/upax/services/EmployeeWorkedHoursCreator.java
@@ -0,0 +1,33 @@
+package mx.com.upax.services;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import mx.com.upax.db.daos.NewEmployeeWorkedHourDAO;
+
+@WebService(serviceName = "EmployeeWorkedHoursCreator")
+public class EmployeeWorkedHoursCreator {
+  private NewEmployeeWorkedHourDAO newEmployeeWorkedHourDAO;
+
+  @WebMethod(operationName = "create")
+  public String create(
+    @WebParam(name = "employee_id") int employeeId,
+    @WebParam(name = "worked_hours") int workedHours,
+    @WebParam(name = "worked_date") String workedDate
+  ) {
+      try {
+        this.newEmployeeWorkedHourDAO =
+          new NewEmployeeWorkedHourDAO(employeeId, workedHours, workedDate);
+        return getResponse();
+      } catch(Exception e) {
+        return "Ha ocurrido un error: " + e.getMessage();
+      }
+  }
+
+  private String getResponse() throws Exception {
+    if(this.newEmployeeWorkedHourDAO.create())
+      return "Horas trabajadas añadidas exitosamente";
+    else
+      return this.newEmployeeWorkedHourDAO.errorsMessage();
+  }
+}

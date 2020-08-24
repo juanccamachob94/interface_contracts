d75327f4a2696627ab2503ce6b3ebe477afad52c
Juan Camacho <juanccamachob94@gmail.com>
2020-08-23 23:13:04 -0500

    unneccessary comments removed

--- src/java/mx/com/upax/utilities/HibernateUtil.java
+++ src/java/mx/com/upax/utilities/HibernateUtil.java
@@ -1,6 +1,5 @@
 package mx.com.upax.utilities;
 
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.hibernate.cfg.AnnotationConfiguration;
--- src/java/mx/com/upax/utilities/NewHibernateUtil.java
+++ src/java/mx/com/upax/utilities/NewHibernateUtil.java
@@ -6,23 +6,22 @@ import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
 
 public class NewHibernateUtil {
+  private static final SessionFactory sessionFactory;
 
-    private static final SessionFactory sessionFactory;
-
-    static {
-      try {
-          Logger mongoLogger = Logger.getLogger("org.hibernate");
-          mongoLogger.setLevel(Level.SEVERE);
-          sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
-      } catch (Throwable e) {
-          System.err.println(
-            "NewHibernateUtil : { Ha fallado la instancia de la SessionFactory " + e + " }"
-          );
-          throw new ExceptionInInitializerError(e);
-      }
+  static {
+    try {
+      Logger mongoLogger = Logger.getLogger("org.hibernate");
+      mongoLogger.setLevel(Level.SEVERE);
+      sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+    } catch (Throwable e) {
+      System.err.println(
+        "NewHibernateUtil : { Ha fallado la instancia de la SessionFactory " + e + " }"
+      );
+      throw new ExceptionInInitializerError(e);
     }
+  }
 
-    public static SessionFactory getSessionFactory() {
-      return sessionFactory;
-    }
+  public static SessionFactory getSessionFactory() {
+    return sessionFactory;
+  }
 }
